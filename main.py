417f7aafb90af539714e589642b680f7c471ba06
Jonah Yordy <jyordy1@liberty.edu>
2023-03-19 21:42:56 -0400

    addded youtube support

--- main.py
+++ main.py
@@ -93,15 +93,15 @@ while True:
         print("No drives detected")
 
     elif drives[0] == "SOUTHPARK":
-        clip_len = 50
+        clip_len = 63
         vname = "curClip.mp4"
         cname = "#cartman #southpark #foryou #fyp #kenny #kyle #game.mp4"
-        videos_folder = os.listdir("/media/"+ os.getlogin() +"/SOUTHPARK/videos")
+        videos_folder = os.listdir("/media/"+ os.getlogin() +"/"+drives[0]+"/videos")
 
         while len(videos_folder) > 0:
             if not os.path.isfile(vname):
                 cur_file = videos_folder[0]
-                shutil.move("/media/"+ os.getlogin() +"/SOUTHPARK/videos/" + cur_file, "./" + vname)
+                shutil.move("/media/"+ os.getlogin() +"/"+drives[0]+"/videos/" + cur_file, "./" + vname)
                 with open("info.pkl", "wb") as f:
                     pickle.dump(0, f)
 
@@ -126,20 +126,15 @@ while True:
 
             # Delete local video file and destroy movie py objects to free up memory
             cur_video.destroy()
-            videos_folder = os.listdir("/media/"+ os.getlogin() +"/SOUTHPARK/videos")
+            videos_folder = os.listdir("/media/"+ os.getlogin() +"/"+drives[0]+"/videos")
             time.sleep(10)
             
     elif drives[0] == "YOUTUBE":
         clip_len = 65
         cname = "#youtube #youtubeclips #foryou #fyp #trending #movie #game #viral.mp4"
-
-    elif drives[0] == "RNM":
-        # Set constants for rick and morty account
-        clip_len = 50
         vname = "curClip.mp4"
-        cname = "#rick #rickandmorty #foryou #fyp #morty #movie #game #❤️❤️.mp4"
 
-        videos_folder = os.listdir("/media/"+ os.getlogin() +"/RNM/videos")
+        videos_folder = os.listdir("/media/"+ os.getlogin() +"/"+drives[0]+"/videos")
 
         # Loop through all videos on drive
         while len(videos_folder) > 0:
@@ -147,7 +142,52 @@ while True:
             if not os.path.isfile(vname):
                 # Move new video from drive to local folder saved as name (vname)
                 cur_file = videos_folder[0]
-                shutil.move("/media/"+ os.getlogin() +"/RNM/videos/" + cur_file, "./" + vname)
+                shutil.move("/media/"+ os.getlogin() +"/"+drives[0]+"videos/" + cur_file, "./" + vname)
+                
+                # Reset pickle file
+                with open("info.pkl", "wb") as f:
+                    pickle.dump(0, f)
+
+            # Init tik tok video object
+            cur_video = TikVideo(vname, cname)
+            print(cur_video.start)
+            
+            # Continue posting clips until entire video is posted
+            while not cur_video.is_over():
+                # Get the current time to check if it is time to post
+                time_now = datetime.datetime.now()
+                print(time_now)
+
+                if [time_now.hour, time_now.minute] in posting_times[time_now.weekday()]:
+                    print("Posting...")
+                    # Create next clip and upload
+                    cur_video.create_next_clip(clip_len)
+                    upload(cname, 1)
+                    # Set new start point for next clip
+                    cur_video.update_info(cur_video.start)
+                    #time.sleep(post_interval + random.randint(0, 600))
+                time.sleep(15)
+
+            # Delete local video file and destroy movie py objects to free up memory
+            cur_video.destroy()
+            videos_folder = os.listdir("/media/"+ os.getlogin() +"/"+drives[0]+"/videos")
+            time.sleep(10)
+
+    elif drives[0] == "RNM":
+        # Set constants for rick and morty account
+        clip_len = 59
+        vname = "curClip.mp4"
+        cname = "#rick #rickandmorty #foryou #fyp #morty #movie #game #❤️❤️.mp4"
+
+        videos_folder = os.listdir("/media/"+ os.getlogin() +"/"+drives[0]+"/videos")
+
+        # Loop through all videos on drive
+        while len(videos_folder) > 0:
+            # Check if we are already editing a video
+            if not os.path.isfile(vname):
+                # Move new video from drive to local folder saved as name (vname)
+                cur_file = videos_folder[0]
+                shutil.move("/media/"+ os.getlogin() +"/"+drives[0]+"videos/" + cur_file, "./" + vname)
                 
                 # Reset pickle file
                 with open("info.pkl", "wb") as f:
@@ -175,7 +215,7 @@ while True:
 
             # Delete local video file and destroy movie py objects to free up memory
             cur_video.destroy()
-            videos_folder = os.listdir("/media/"+ os.getlogin() +"/RNM/videos")
+            videos_folder = os.listdir("/media/"+ os.getlogin() +"/"+drives[0]+"/videos")
             time.sleep(10)
 
     time.sleep(30)
